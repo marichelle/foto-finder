041cd1868b738c5a6f9934eabca36d9cfb7b95e1
Maya Estalilla <mestalilla@gmail.com>
2023-07-30 14:21:12 -0400

    Update image resolution

--- src/components/ImageList.jsx
+++ src/components/ImageList.jsx
@@ -3,10 +3,10 @@ import ImageShow from './ImageShow'
 function ImageList({ images }) {
   return (
     <ul className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-      {images.map(({ id, alt_description, urls: { small } }) => {
+      {images.map(image => {
         return (
-          <li key={id}>
-            <ImageShow src={small} altText={alt_description} />
+          <li key={image.id}>
+            <ImageShow image={image} />
           </li>
         )
       })}
--- src/components/ImageShow.jsx
+++ src/components/ImageShow.jsx
@@ -1,9 +1,14 @@
-function ImageShow({ src, altText }) {
+function ImageShow({ image }) {
+  const {
+    alt_description,
+    urls: { regular },
+  } = image
+
   return (
     <div className="relative group aspect-square block w-full overflow-hidden rounded-lg bg-gray-100">
       <img
-        src={src}
-        alt={altText}
+        src={regular}
+        alt={alt_description}
         className="absolute object-cover object-center w-full h-full pointer-events-none group-hover:opacity-75"
       />
     </div>
